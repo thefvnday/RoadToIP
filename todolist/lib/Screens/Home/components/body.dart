f26bc1013913a2611d46c440b5c01b33d7a8af67
Fandy Ahmad Irianto <fandy.09.ahmad@gmail.com>
2021-10-24 15:53:49 +0700

    update tampilan homescreen dan task

--- todolist/lib/Screens/Home/components/body.dart
+++ todolist/lib/Screens/Home/components/body.dart
@@ -1,16 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/Screens/Home/components/getstarted_card.dart';
 import 'package:todolist/Screens/Home/components/img_header.dart';
 import 'package:todolist/Screens/Home/components/task_card_widget.dart';
 import 'package:todolist/Screens/Home/components/text_header.dart';
+import 'package:todolist/Screens/TaskScreen/task_screen.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Future<void> showInformationDialog(BuildContext context) async {
+    return await showDialog(
+        context: context,
+        builder: (context) {
+          final TextEditingController _textEditingController =
+              TextEditingController();
+          bool isChecked = false;
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              content: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        controller: _textEditingController,
+                        validator: (value) {
+                          return value!.isNotEmpty ? null : "Invalid Field";
+                        },
+                        decoration:
+                            InputDecoration(hintText: "Enter Some Text"),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Choice Box"),
+                          Checkbox(
+                              value: isChecked,
+                              onChanged: (checked) {
+                                setState((){
+                                  isChecked = checked!;
+                                }) ;
+                              })
+                        ],
+                      )
+                    ],
+                  )),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Noted'),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            );
+          });
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black26,
       body: Container(
+        width: double.infinity,
+        height: double.infinity,
         child: ListView(
           children: <Widget>[
             SizedBox(
@@ -49,14 +112,18 @@ class Body extends StatelessWidget {
                     children: [
                       Column(
                         children: [
+                          GetStartedCard(
+                              title1: 'GetStarted!',
+                              note1:
+                                  'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'),
                           TaskCardWidget(
                             title: 'Daily Stand Up',
-                            note: 'Makan makan', 
+                            note: 'Makan makan',
                             date: 'Minggu, 24 Juli 2010',
                           ),
                           TaskCardWidget(
                             title: 'Daily Stand Up',
-                            note: 'Makan makan', 
+                            note: 'Makan makan',
                             date: 'Minggu, 24 Juli 2010',
                           ),
                         ],
@@ -69,52 +136,66 @@ class Body extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
-        
-            // Container(
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //     children: <Widget>[
-            //       Positioned(
-                    
-            //         bottom: 0,
-            //         child: Container(
-            //           width: 50,
-            //           height: 50,
-            //           child: Icon(
-            //             Icons.task,
-            //             color: Colors.blue,
-            //           ),
-            //         ),
-            //       ),
-            //       Positioned(
-            //         bottom: 0,
-            //         child: Container(
-            //           width: 50,
-            //           height: 50,
-            //           decoration: BoxDecoration(
-            //               color: Colors.blue,
-            //               borderRadius: BorderRadius.circular(20)),
-            //           child: Icon(
-            //             Icons.add,
-            //             color: Colors.white,
-            //           ),
-            //         ),
-            //       ),
-            //       Positioned(
-            //         bottom: 0,
-            //         left: 0,
-            //         child: Container(
-            //           width: 50,
-            //           height: 50,
-            //           child: Icon(Icons.settings, color: Colors.blue),
-            //         ),
-            //       )
-            //     ],
-            //   ),
-            // )
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {},
+                    child: Positioned(
+                      bottom: 0,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        child: Icon(
+                          Icons.task,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      await showInformationDialog(context);
+                    },
+                    child: Positioned(
+                      bottom: 0,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Positioned(
+                      bottom: 0,
+                      left: 0,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        child: Icon(
+                          Icons.settings,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
-        ), 
-      ), 
+        ),
+      ),
     );
   }
 }
---
+++ todolist/lib/Screens/Home/components/getstarted_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class GetStartedCard extends StatelessWidget {
+  final String title1;
+  final String note1;
+  const GetStartedCard({ Key? key,required this.title1,required this.note1 }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Container(
+        
+          padding: EdgeInsets.symmetric(
+            vertical: 20,
+            horizontal: 20,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title1,
+                style: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+                  
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text(
+                  note1,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+      ),
+    );
+  }
+}
